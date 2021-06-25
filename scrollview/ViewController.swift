a726a1c053756e2cd9db242979419e5cf7e91d4a
samleedevelop33 <samlee.develop@gmail.com>
2021-06-25 18:25:24 +0900

    init

---
+++ scrollview/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  scrollview
+//
+//  Created by sam on 2020/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //https://www.youtube.com/watch?v=KmE50giVuLA
+        //유튜브이거보고했음
+    }
+
+
+}
+
